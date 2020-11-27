8b2d9fb35228bce13629a9347904f9bfcb78bae0
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-11-27 10:55:59 +0300

    Add skv_mem binary
    Add example of using library.

---
+++ src/skv_mem.rs
@@ -0,0 +1,38 @@
+use libsimplekv::SimpleKV;
+
+const USAGE: &str = "
+Usage:
+    skv_mem FILE get KEY
+    skv_mem FILE delete KEY
+    skv_mem FILE insert KEY VALUE
+    skv_mem FILE update KEY VALUE
+";
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let fname = args.get(1).expect(&USAGE);
+    let action = args.get(2).expect(&USAGE).as_ref();
+    let key = args.get(3).expect(&USAGE).as_ref();
+    let maybe_value = args.get(4);
+
+    let path = std::path::Path::new(&fname);
+    let mut store = SimpleKV::open(path).expect("unable to open file");
+    store.load().expect("unable to load data");
+
+    match action {
+        "get" => match store.get(key).unwrap() {
+            None => eprintln!("{:?} not found", key),
+            Some(value) => println!("{:?}", value),
+        },
+        "delete" => store.delete(key).unwrap(),
+        "insert" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.insert(key, value).unwrap()
+        }
+        "update" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.update(key, value).unwrap()
+        }
+        _ => eprintln!("{}", &USAGE),
+    }
+}
